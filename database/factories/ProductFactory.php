08d7dddb0060a081b451e3cbb5dd4a1e93775b98
MinhazDeveloper <minhazt14@gmail.com>
2025-08-19 15:54:51 +0600

    Seeder for product

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -16,8 +16,13 @@ class ProductFactory extends Factory
      */
     public function definition(): array
     {
+        
         return [
-            //
+            'name' => $this->faker->word(), // পণ্যের নাম
+            'detail' => $this->faker->sentence(10), // ১০ শব্দের ডিটেইল
+            'price' => $this->faker->numberBetween(100, 10000), // ১০০-১০,০০০ টাকার মধ্যে
+            'stock' => $this->faker->numberBetween(1, 100), // ১-১০০ পিস স্টক
+            'discount' => $this->faker->numberBetween(0, 50), // ০-৫০% ডিসকাউন্ট
         ];
     }
 }
