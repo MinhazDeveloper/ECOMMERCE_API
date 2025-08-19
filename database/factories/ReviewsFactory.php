e9ef99c525368e614410624b2aac2024a9bfd149
MinhazDeveloper <minhazt14@gmail.com>
2025-08-19 16:15:59 +0600

    Seeder for reviews

--- database/factories/ReviewsFactory.php
+++ database/factories/ReviewsFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Product;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Reviews>
@@ -17,7 +18,11 @@ class ReviewsFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'product_id' => Product::inRandomOrder()->first()->id ?? Product::factory(), 
+            // যদি আগে থেকে Product থাকে তাহলে random product_id দিবে
+            'customer' => $this->faker->name(),
+            'review' => $this->faker->sentence(20), // ২০ শব্দের রিভিউ
+            'star' => $this->faker->numberBetween(1, 5), // রেটিং ১-৫ এর মধ্যে
         ];
     }
 }
--- database/seeders/ReviewsSeeder.php
+++ database/seeders/ReviewsSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Reviews;
 
 class ReviewsSeeder extends Seeder
 {
@@ -12,6 +13,6 @@ class ReviewsSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+          Reviews::factory()->count(100)->create();
     }
 }
